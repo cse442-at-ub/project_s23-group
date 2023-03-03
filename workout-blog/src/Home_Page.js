0acd9b2368c6d9f44d26e4d6e647cbf615abce58
Sander Shi <sandershi34@gmail.com>
2023-03-03 12:39:20 -0500

    create navbar

--- workout-blog/src/Home_Page.js
+++ workout-blog/src/Home_Page.js
@@ -2,13 +2,35 @@ import React from "react";
 
 function Home_Page(){
     return (
-        <div>
-            <nav class="navbar">
-                <a class="navbar-brand" href="/">GYM BLOG</a>
-            </nav>
-         </div>
+        <div className="container">
+            <div className="navbar" >
+                <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+                    <a className="navbar-brand" href="#">Gym Blog</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Profile<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Post</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign Up</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign in</a>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
     );
 
 }
+
 
 export default Home_Page;
