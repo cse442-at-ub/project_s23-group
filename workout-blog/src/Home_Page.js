75842d283b02668e6049a5f1723209fd10bf28a3
Sander Shi <sandershi34@gmail.com>
2023-03-03 09:49:03 -0500

    initialized homepage

---
+++ workout-blog/src/Home_Page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home_Page(){
+    return (
+        <div>
+            <nav class="navbar">
+                <a class="navbar-brand" href="/">GYM BLOG</a>
+            </nav>
+         </div>
+    );
+
+}
+
+export default Home_Page;
