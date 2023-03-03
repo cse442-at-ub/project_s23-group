e9e473d9c60d3dbdbe6fd0a5ace847ff52e64639
etomiello <eftomiello@gmail.com>
2023-03-02 22:34:20 -0500

    Added login page, need to update buttons

--- workout-blog/src/App.js
+++ workout-blog/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import downloadImage from './download.jpg';
 
 function App() {
+  function handleSignIn() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    // Add code here to handle the sign-in action using the username and password values
+    //First take the username and check if thats in the database
+    //If it is then check the password of that username and if it matches the password typed by the user bring them to their account
+    window.location.href = "https://ublearns.buffalo.edu/?new_loc=%2Fultra%2Finstitution-page";
+  }//temp link
+  function forgotPass() {
+    window.location.href = "https://ublearns.buffalo.edu/?new_loc=%2Fultra%2Finstitution-page";
+  }// temp link
+  function createAccount() {
+    window.location.href = "https://ublearns.buffalo.edu/?new_loc=%2Fultra%2Finstitution-page";
+  }// temp link
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id="myImage" src={downloadImage} alt="Login Image"/>
+      <div className="Sign in">
+        <h1>Sign in</h1>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button onClick={handleSignIn}>Sign in</button>
+        <button onClick={forgotPass}>Forgot your password?</button>
+        <button onClick={createAccount}>Dont have an Account? Create one!</button>      
+        </div>
     </div>
   );
 }
 
 export default App;
+
