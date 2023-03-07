150053dc6e6690e6d8c64f6adfe25ad673b3e080
Sander Shi <sandershi34@gmail.com>
2023-03-07 13:59:56 -0500

    add card image

--- workout-blog/src/Home_Page.js
+++ workout-blog/src/Home_Page.js
@@ -1,32 +1,28 @@
 import React from "react";
+import image from "./Image/cbum.jpg"
+import './Home_Page.css'
 
 function Home_Page(){
     return (
-        <div className="container">
-            <div className="navbar" >
-                <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-                    <a className="navbar-brand" href="#">Gym Blog</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Profile<span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Post</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Sign Up</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Sign in</a>
-                        </li>
-                        </ul>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light col-12">
+                <a class="navbar-brand" href="#">Gym Blog</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse col-11" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link" href="#">Profile</a>
+                        <a class="nav-item nav-link" href="#">Post</a>
+                        <a class="nav-item nav-link" href="#">Sign Up</a>
+                        <a class="nav-item nav-link" href="#">Sign In</a>
                     </div>
-                </nav>
+                </div>
+            </nav>
+            <div class="card col-8 ml-auto mr-auto">
+                <img src = {image}/>
             </div>
+            
         </div>
     );
 
