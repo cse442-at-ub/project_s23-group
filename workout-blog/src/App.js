8ebe8c82a382b9c969170b13f0520078b860c387
etomiello <eftomiello@gmail.com>
2023-03-13 13:06:08 -0400

    connected login signup #33

---
+++ workout-blog/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Login from '/Users/erictomiello/Desktop/School code/442 code/Connect_log_sign/workout-blog/src/login/login.js';
+import Register from './components/Register';
+import './App.css';
+
+function App() {
+  const [form, setForm] = useState('login');
+
+  const handleFormSwitch = (newForm) => {
+    setForm(newForm);
+  }
+
+  return (
+    <div className="App">
+      {form === 'login' ? <Login onFormSwitch={handleFormSwitch} /> : <Register onFormSwitch={handleFormSwitch} />}
+    </div>
+  );
+}
+
+export default App;
