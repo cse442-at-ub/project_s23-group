da45a81d8159765cb57c40a7f52f23295632e847
schaefe2 <60619953+schaefe2@users.noreply.github.com>
2023-03-18 16:54:02 -0400

    added password hashing

--- workout-blog/src/login.php
+++ workout-blog/src/login.php
@@ -8,7 +8,7 @@ if(isset($email) && isset($password)){
     $userinfo = checkLogin($email);
     $login = false;
     if(isset($userinfo)){
-        if($userinfo[3] != $password){
+        if($userinfo[3] != password_hash($password,PASSWORD_DEFAULT)){
             http_response_code(400);
             exit;
         }
