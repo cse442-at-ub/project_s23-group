e84d217cafc476bc279d1a0c3019c3c40035139e
Sander Shi <sandershi34@gmail.com>
2023-03-18 22:02:05 -0400

    fix javascript #40

--- workout-blog/src/App.js
+++ workout-blog/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import Login from '/Users/erictomiello/Desktop/School code/442 code/project_s23-group/workout-blog/src/login/login.js';
+import Login from './login/login.js';
 import Register from './components/Register';
 import Home from './home/Home_Page';
 import './App.css';
 
 function App() {
+  
   const [form, setForm] = useState(null);
 
   const handleFormSwitch = (newForm) => {
